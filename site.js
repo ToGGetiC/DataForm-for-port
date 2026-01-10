2116d84be61aebcbebe6cfdb1f1de2bdd9d85b59
ToGGetiC <artyom.satlaev@yandex.ru>
2026-01-10 23:29:15 +0700

    Add files via upload

---
+++ site.js
@@ -0,0 +1,37 @@
+const SUPABASE_URL = 'https://seyjvxfxrxaanrtuvcoz.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_-L-xk1i5gA0NigVphnBSKQ_42qxd3Zb';
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+const form = document.getElementById('feedbackForm');
+form.addEventListener('submit', async(e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const message = document.getElementById('message').value;
+
+    const { data, error } = await supabase
+        .from('feedback')
+        .insert([{ name, message }]);
+
+    if (error) {
+        alert('Ошибка при отправке данных: ' + error.message);
+    } else {
+        alert('Данные успешно отправлены!');
+        form.reset();
+    }
+});
+
+async function exportFromDB() {
+    const { data, error } = await supabase
+        .from('feedback')
+        .select('*');
+    if (error) {
+        alert('Ошибка при получении данных: ' + error.message);
+        return;
+    }
+
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Feedback');
+
+    XLSX.writeFile(workbook, 'feedback_data.xlsx');
+}
